9e3a901541ea40039473acae891466e09a2304fc
alix14856 <alix14856@gmail.com.com>
2026-03-17 12:27:48 +0300

    Final fix: rewrites for sitemap.xml to route handler

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
-        source: '/sitemap.xml',
-        destination: '/sitemap.xml/route',
-      },
-      {
-        source: '/robots.txt',
-        destination: '/robots.txt/route', // إذا كنت ستستخدم Route Handler للروبوتس أيضاً
+        source: '/sitemap.xml', // الرابط الذي يطلبه جوجل (لا تغيره)
+        destination: '/sitemap', // اسم المجلد الذي أنشأته (app/sitemap)
       },
     ];
   },
