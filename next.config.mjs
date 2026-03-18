8542cd8cf44bdaf824ff9873f89b5676f228c03d
alix14856 <alix14856@gmail.com.com>
2026-03-18 10:10:48 +0300

    Update my tools

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // حذفنا قسم eslint لأنه يسبب الخطأ الذي رأيته
   typescript: { 
     ignoreBuildErrors: true 
   },
   images: { 
     unoptimized: true 
   },
-  // هذا التوجيه هو الذي سينهي مشكلة "الصفحة السوداء" للخريطة
   async rewrites() {
     return [
       {
         source: '/sitemap.xml',
-        destination: '/mysitemap.xml', // تأكد أن هذا الملف موجود في مجلد public
+        destination: '/app/sitemap.xml', // الملف موجود داخل مجلد app
       },
     ];
   },
